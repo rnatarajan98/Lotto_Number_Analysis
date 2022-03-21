05b4ea7eff14f33582f27d076b21542d5c9cb82c
richardnatarajan <richard.natarajan@tektowr.com>
2022-03-21 14:39:39 +0000

    added result accessor functions

--- result.py
+++ result.py
@@ -6,3 +6,15 @@ class Result:
         for b in row[1:8]:
             self.Balls.append(b)
 
+    def get_date(self):
+        return self.Date
+
+    def get_balls6(self):
+        return self.Balls[0:6]
+
+    def get_balls7(self):
+        return self.Balls[0:7]
+
+    def get_powerball(self):
+        return self.Balls[6]
+
